6a63bc24c268059cbe599a5b57eb50321b19f944
seohyoseok0408 <seohyoseok0408@gmail.com>
2024-07-31 02:54:13 +0900

    refactor: 기존 로그인 방식 코드 삭제 + 회원가입 중복Id찾기

--- src/main/java/com/smu/board/controller/UserApiController.java
+++ src/main/java/com/smu/board/controller/UserApiController.java
@@ -1,33 +1,44 @@
 package com.smu.board.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.smu.board.dto.ResponseDto;
 import com.smu.board.model.User;
 import com.smu.board.service.UserService;
 
-@RestController
-public class UserApiController {
-	
-	@Autowired
-	private UserService userService;
-	
-	@PostMapping("/auth/signup")
-	public ResponseDto<Integer> save(@RequestBody User user){
-		System.out.println("UserApiController : save 호출됨");
-		userService.회원가입(user);
-		return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
-		// HTTP 상태코드, 커스텀 코드
-	}
-}
+import jakarta.servlet.http.HttpSession;
 
-//{
-//    "email": "example@example.com",
-//    "name": "Example User",
-//    "password": "password123",
-//    "userType": "STUDENT"
-//}
+@RestController
+@RequestMapping("/auth")
+public class UserApiController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @PostMapping("/signup")
+    public ResponseDto<Integer> save(@RequestBody User user) {
+        System.out.println("UserApiController: save 호출됨");
+        int result = userService.회원가입(user);
+        System.out.println("UserApiController: 회원가입 결과 : " + result);
+        return new ResponseDto<Integer>(HttpStatus.OK.value(), result); 
+    }
+
+}
--- src/main/java/com/smu/board/service/UserService.java
+++ src/main/java/com/smu/board/service/UserService.java
@@ -4,6 +4,8 @@ import com.smu.board.model.RoleType;
 import com.smu.board.model.User;
 import com.smu.board.repository.UserRepository;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -12,37 +14,36 @@ import org.springframework.transaction.annotation.Transactional;
 // 스프링이 컴포넌트 스캔을 통해 Bean 등록
 @Service
 public class UserService {
-    
-	@Autowired
-	private UserRepository userRepository;
-    
-	@Autowired
-	private BCryptPasswordEncoder encoder;
 	
-	@Transactional
-    public int 회원가입(User user) {
-    	System.out.println("userService의 로그인 함수 실행됨");
-    	String rawPassword = user.getPassword();
-    	String encPassword = encoder.encode(rawPassword);
-    	user.setPassword(encPassword);
-    	user.setRole(RoleType.USER);
-    	try {
-    		userRepository.save(user);
-    		return 1;
-    	} catch (Exception e) {
-    		return -1;
-    	}
-    	
-    }
+	private final UserRepository userRepository;
+	private final BCryptPasswordEncoder encoder;
     
+	public UserService(UserRepository userRepository, BCryptPasswordEncoder encoder) {
+		this.userRepository = userRepository;
+		this.encoder = encoder;
+	}
+	
+    @Transactional
+    public int 회원가입(User user) {
+        System.out.println("UserService: 회원가입 함수 실행됨");
+        
+        Boolean isExist = userRepository.existsByUsername(user.getUsername()); // 이메일이 존재하는지 확인
+        
+        if (isExist) { // 이메일이 이미 존재함
+        	return -1;
+        }
+        
+        String rawPassword = user.getPassword();
+        String encPassword = encoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        user.setRole(RoleType.USER);
+        try {
+            userRepository.save(user);
+            return 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
 }
-//@Transactional
-//public User 회원가입(User user) {
-//    System.out.println("userService의 회원가입 함수 실행됨");
-//    Optional<User> existingUser = userRepository.findByEmail(user.getEmail());
-//    if (existingUser.isPresent()) {
-//        throw new IllegalArgumentException("이미 존재하는 이메일입니다.");
-//    }
-//    user.setRole(RoleType.USER);
-//    return userRepository.save(user);
-//}
